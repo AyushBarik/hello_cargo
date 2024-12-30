3610cff2ab0f2d9c53d35e3d8974b48dce059f5d
Ayush Barik <coolayush2017@gmail.com>
2024-12-29 21:28:50 -0800

    neural network finished - rust

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,72 @@
-use std::io; //io is a module
-use rand::Rng; //rng is a trait
-use std::cmp::Ordering;
+// use std::arch::aarch64::float32x2_t;
+ use std::io; //io is a module
+// use rand::Rng; //rng is a trait
+// use std::cmp::Ordering;
+// use std::mem;
 
 fn main() {
-    println!("guess!");
-    let secret = rand::thread_rng().gen_range(1..=100);
-    loop {
-    let mut guess = String::new(); //new is a function of the String struct
+
+    let mut b: f32 = 0.0;
+    let mut m: f32 = 1.0;
+    const epochs: i32 = 10000;
+    //const trainStep: f32 = 0.01;
+    //y = mx+b (minimize this loss)
+
+    const size: usize = 9;
+    const sizeF: f32 = size as f32;
+
+    let _data: [(f32, f32); size] = [(1.0,1.0),(2.0,2.0),(3.0,3.0),(4.0,4.0),(5.0,5.0),(6.0,6.0),(7.0,7.0),(8.0,8.0),(9.0,9.0)];
+    let input: [f32; size] = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0];
+
+   // let mut loss: Vec<f32> = vec![];
+
+    for u in 0..epochs{
+    let mut output: Vec<f32> = vec![];
+    //output matrix
+    for i in input{
+        let out = i*m + b;
+        output.push(out);
+        //println!("{out}");
+    }
+
+    //println!("done");
+
+    //total loss (1/size)E(y - (mx+b))^2
+    let mut tloss = 0.0;
+    for j in 0..size{
+        tloss += (_data[j].1 - output[j])*(_data[j].1 - output[j]);
+        //println!("{tloss}");
+    }
+    tloss = tloss / sizeF;
+    //println!("tloss {tloss}");
+
+    //loss function of m
+    //d/dm = (2/size)E(y - (mx + b))(-x)
+    let mut mloss: f32 = 0.0;
+    for g in 0..size{
+        mloss += (2.0 / sizeF)*(_data[g].1 - output[g])*(-1.0 * input[g]);
+    }
+    //println!("mloss -- {mloss}");
+    
+    m -= 0.01*mloss;
+
+    //loss function of b
+    //d/db = (2/size)(y - (mx + b))
+    let mut bloss: f32 = 0.0;
+    for h in 0..size{
+        bloss += (2.0 / sizeF)*(_data[h].1 - output[h])*(-1.0);
+    }
+    //println!("bloss -- {bloss}");
+
+    b -= 0.01*bloss;
     
 
-    io::stdin() //returns Result Enum - this is what .expect looks at
-        .read_line(&mut guess) //& means reference, not address like in C, immutable by default
-        .expect("Failed"); //error handling
-    //same as io::stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess: i32 = match guess.trim().parse(){
-        Ok(num) => num,
-        Err(_) => continue, //underscore is catch all
-
-    }; 
-
-    match guess.cmp(&secret){
-        Ordering::Less => println!("too small"),
-        Ordering::Equal => {
-            println!("nice"); 
-            break;
-        },
-        Ordering::Greater => println!("too big"),
-    }
-
-    println!("You guessed {guess}");
-    let distance = secret - guess;
-    println!("you were {distance} away!\n answer was {secret}");
-    }
-
+    
 }
 
-//SemVer automatically used in .toml 
+    println!("Slope -- {m}");
+    println!("Y-intercept -- {b}");
 
-/* example:
-let x = 5;
-let y = 10;
 
-println!("x = {x} and y + 2 = {}", y + 2);
- */ 
+
+}
